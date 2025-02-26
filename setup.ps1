3cac4c79529d1a4eacdd691e930f798091288732
elmousse <leo.mousset@outlook.fr>
2025-02-26 20:53:34 +0100

    update script

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,5 @@
-# Prompt the user for the project name
-$projectName = Read-Host "Enter the project name"
+$projectName = Read-Host "Project name"
 
-# Define file paths to be updated
 $filesToUpdate = @(
     "Properties\AssemblyInfo.cs",
     "JumpKingModTemplate.sln",
@@ -9,17 +7,16 @@ $filesToUpdate = @(
     "ModEntry.cs"
 )
 
-# Replace "JumpKingModTemplate" with the project name in the specified files
 foreach ($file in $filesToUpdate) {
     if (Test-Path $file) {
         (Get-Content $file) | ForEach-Object { $_ -replace "JumpKingModTemplate", $projectName } | Set-Content $file
         Write-Host "Updated $file with project name"
-    } else {
+    }
+    else {
         Write-Host "File not found: $file"
     }
 }
 
-# Rename the .sln and .csproj files
 $slnFile = "./JumpKingModTemplate.sln"
 $csprojFile = "./JumpKingModTemplate.csproj"
 if (Test-Path $slnFile) {
@@ -32,16 +29,16 @@ if (Test-Path $csprojFile) {
     Write-Host "Renamed JumpKingModTemplate.csproj to $projectName.csproj"
 }
 
-# Prompt for the level debug path
-$debugPath = Read-Host "Enter the level debug path (e.g., C:\path\to\levels\bin)"
+$debugPath = Read-Host "Path for level debug (C:\path\to\level\bin\)"
 
-# Update the .run/Debug.run.xml with the provided debug path
 $runXmlPath = ".run\Debug.run.xml"
 if (Test-Path $runXmlPath) {
     (Get-Content $runXmlPath) | ForEach-Object { $_ -replace "C:\\Users\\elmousse\\Documents\\_pv\\jump king\\levels\\mylevel\\bin\\", $debugPath } | Set-Content $runXmlPath
     Write-Host "Updated $runXmlPath with the provided debug path"
-} else {
+}
+else {
     Write-Host "File not found: $runXmlPath"
 }
 
-Write-Host "Script completed successfully!"
+Write-Host "Project ready!"
+
